5a3dddb8b5b71018c9aaad27d426bdc4c66af6cc
Theo Henrique <theo123b@gmail.com>
2024-03-30 18:04:24 -0300

    explicações rm

--- rm/rm.c
+++ rm/rm.c
@@ -2,12 +2,17 @@
 #include <dirent.h>
 
 int main(){
+    //abrindo diretório
     struct dirent *d;
     DIR *dr;
+    //nesse buf, vamos colocar o endereço de cada arquivo do diretório (diretorio/arquivo)
     char buf[256];
-    char nome_arq[10];
+    //nome do diretorio
+    char nome_arq[10]; 
     scanf("%s", nome_arq);
+    //abrindo o diretório no dir
     dr = opendir(nome_arq);
+    //percorrendo o diretório e removendo cada arquivo
     while((d = readdir(dr)) != NULL){
       sprintf(buf, "%s/%s", nome_arq, d->d_name);
       remove(buf);
